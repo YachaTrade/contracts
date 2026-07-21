6455b2a7ffcbf2066d7495585f547107d5c364c5
kong <udt6236@gmail.com>
2026-07-21 16:42:18 +0900

    test: cover LP manager V3 allocation lifecycle

--- test/modules/LPManagerV3.t.sol
+++ test/modules/LPManagerV3.t.sol
@@ -10,10 +10,31 @@ import {TickMath} from "@uniswap/v3-core/contracts/libraries/TickMath.sol";
 import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
 import {IAuthority} from "@openzeppelin/contracts/access/manager/IAuthority.sol";
 import {IV3LiquidityActor} from "../../src/interfaces/IV3LiquidityActor.sol";
+import {IProtocolManager} from "../../src/interfaces/IProtocolManager.sol";
 import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract MockAuthority is IAuthority {
     mapping(bytes32 => bool) public allowed;
+    mapping(address => IProtocolManager.QuoteConfig) public configs;
+    address public receiver;
+
+    function setReceiver(address r) external {
+        receiver = r;
+    }
+
+    function setConfig(address q, IProtocolManager.QuoteConfig calldata c) external {
+        configs[q] = c;
+    }
+
+    function getConfig(address q) external view returns (IProtocolManager.QuoteConfig memory) {
+        return configs[q];
+    }
+
+    function feeReceiver() external view returns (address) {
+        return receiver;
+    }
 
     function set(address caller, address target, bytes4 selector, bool value) external {
         allowed[keccak256(abi.encode(caller, target, selector))] = value;
@@ -24,31 +45,104 @@ contract MockAuthority is IAuthority {
     }
 }
 
-contract MockFactory {}
+contract MockFactory {
+    address public pool;
+
+    function setPool(address p) external {
+        pool = p;
+    }
+
+    function getPool(address, address, uint24) external view returns (address) {
+        return pool;
+    }
+}
+
+contract MockERC20 is ERC20 {
+    constructor(string memory n) ERC20(n, n) {}
+
+    function mint(address to, uint256 a) external {
+        _mint(to, a);
+    }
+}
+
+contract MockRegistry {
+    ITokenRegistry.TokenInfo public info;
+
+    function setInfo(ITokenRegistry.TokenInfo calldata i) external {
+        info = i;
+    }
+
+    function getTokenInfo(address) external view returns (ITokenRegistry.TokenInfo memory) {
+        return info;
+    }
+
+    function getPair(address) external view returns (address) {
+        return info.pair;
+    }
+}
+
+contract MockPool {
+    address public token0;
+    address public token1;
+    address public factory;
+    uint24 public fee;
+    int24 public tickSpacing;
+
+    constructor(address a, address b, address f, uint24 ft, int24 s) {
+        token0 = a;
+        token1 = b;
+        factory = f;
+        fee = ft;
+        tickSpacing = s;
+    }
+
+    function slot0() external pure returns (uint160, int24, uint16, uint16, uint16, uint8, bool) {
+        return (1 << 96, 0, 0, 0, 0, 0, true);
+    }
+}
 
 contract MockActor is IV3LiquidityActor {
     address public immutable override owner;
     address public immutable override factory;
+    uint256 public used0;
+    uint256 public used1;
+    bool public shouldRevert;
+    bytes32 public qpos;
+    bytes32 public tpos;
 
     constructor(address owner_, address factory_) {
         owner = owner_;
         factory = factory_;
     }
 
-    function quoteLiquidityPositions(address) external pure returns (bytes32, int24, int24, uint128) {
-        return (bytes32(0), 0, 0, 0);
+    function quoteLiquidityPositions(address) external view returns (bytes32, int24, int24, uint128) {
+        return (qpos, 0, 0, 1);
     }
 
-    function tokenLiquidityPositions(address) external pure returns (bytes32, int24, int24, uint128) {
-        return (bytes32(0), 0, 0, 0);
+    function tokenLiquidityPositions(address) external view returns (bytes32, int24, int24, uint128) {
+        return (tpos, 0, 0, 1);
     }
 
-    function mint(ILPManager.PoolData calldata, uint256, uint256) external pure returns (uint256, uint256) {
-        return (0, 0);
+    function setUsage(uint256 a, uint256 b, bool r) external {
+        used0 = a;
+        used1 = b;
+        shouldRevert = r;
     }
 
-    function increase(ILPManager.PoolData calldata, uint256, uint256) external pure returns (uint256, uint256) {
-        return (0, 0);
+    function mint(ILPManager.PoolData calldata d, uint256 a0, uint256 a1) external returns (uint256, uint256) {
+        if (shouldRevert) revert();
+        IERC20(d.token0).transferFrom(msg.sender, address(this), used0);
+        IERC20(d.token1).transferFrom(msg.sender, address(this), used1);
+        qpos = bytes32(uint256(1));
+        tpos = bytes32(uint256(2));
+        return (used0, used1);
+    }
+
+    function increase(ILPManager.PoolData calldata d, uint256 a0, uint256 a1) external returns (uint256, uint256) {
+        if (shouldRevert) revert();
+        IERC20(d.token0).transferFrom(msg.sender, address(this), used0);
+        IERC20(d.token1).transferFrom(msg.sender, address(this), used1);
+        return (used0, used1);
     }
 
     function collectFees(address) external pure returns (uint256, uint256) {
@@ -62,6 +156,54 @@ contract MockActor is IV3LiquidityActor {
 
     /// Focused API/math smoke tests. Full lifecycle fixtures are covered by integration suites.
     contract LPManagerV3Test is Test {
+        function _fixture(bool quote0) internal returns (LPManager, MockERC20, MockERC20, MockActor) {
+            MockAuthority a = new MockAuthority();
+            MockRegistry r = new MockRegistry();
+            MockFactory f = new MockFactory();
+            MockERC20 token = new MockERC20("T");
+            MockERC20 quote = new MockERC20("Q");
+            address t0 = quote0 ? address(quote) : address(token);
+            address t1 = quote0 ? address(token) : address(quote);
+            MockPool p = new MockPool(t0, t1, address(f), 3000, 60);
+            f.setPool(address(p));
+            r.setInfo(
+                ITokenRegistry.TokenInfo(address(p), address(p), address(quote), ITokenRegistry.DexType.UniswapV3, 3000)
+            );
+            IProtocolManager.QuoteConfig memory c;
+            c.active = true;
+            c.v3FeeTier = 3000;
+            a.setConfig(address(quote), c);
+            a.setReceiver(address(0xBEEF));
+            LPManager m = LPManager(
+                address(
+                    new ERC1967Proxy(
+                        address(new LPManager()), abi.encodeCall(LPManager.initialize, (address(a), address(r)))
+                    )
+                )
+            );
+            MockActor actor = new MockActor(address(m), address(f));
+            a.set(address(this), address(m), m.setV3LiquidityActor.selector, true);
+            m.setV3LiquidityActor(address(actor), address(f));
+            a.set(address(this), address(m), m.allocate.selector, true);
+            a.set(address(this), address(m), m.increaseLiquidity.selector, true);
+            token.mint(address(m), 1000);
+            quote.mint(address(m), 1000);
+            return (m, token, quote, actor);
+        }
+
+        function test_allocate_and_increase_harness() public {
+            (LPManager m, MockERC20 token, MockERC20 quote, MockActor actor) = _fixture(true);
+            actor.setUsage(70, 50, false);
+            ILPManager.AllocateParams memory p = ILPManager.AllocateParams(address(token), 100, 200, 1000, 2000, 1);
+            m.allocate(p);
+            assertEq(token.allowance(address(m), address(actor)), 0);
+            assertEq(quote.allowance(address(m), address(actor)), 0);
+            actor.setUsage(10, 5, false);
+            m.increaseLiquidity(address(token), 20, 30);
+            (bytes32 q,,,,,,,) = m.getPositions(address(token));
+            assertEq(q, bytes32(uint256(1)));
+        }
+
         function test_setV3LiquidityActor_wiresOnceAndValidates() public {
             MockAuthority authority = new MockAuthority();
             LPManager implementation = new LPManager();
